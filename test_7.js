ed2cfbd284edfcb55ae38bc908ac90390404dd85
Anastasiya Pazniak <pazniak.anastasiya.24@gmail.com>
2025-09-22 09:50:17 +0300

    complete the task output prime numbers

---
+++ test_7.js
@@ -0,0 +1,13 @@
+let n = prompt("Enter a number", 1);
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+    
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            continue nextPrime;
+        }
+    }
+
+    alert(i);
+}
